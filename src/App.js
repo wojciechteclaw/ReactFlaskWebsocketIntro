0a3d7f361cbdad46095c8e013aa32dbf514c2ef6
Wojciech Teclaw <teclaw.wojciech@gmail.com>
2022-01-15 13:49:41 +0100

    Add reference hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useRef} from "react";
 import {io} from "socket.io-client";
 
 var socket = io("ws://127.0.0.1:5000")
@@ -7,23 +7,26 @@ const App = () => {
     const [messages, setMessages] = useState([]);
     const [message, setMessage] = useState("");
     const [messagesHTML, setMessagesHTML] = useState([]);
+    const lastestMessages = useRef(null);
+
+    lastestMessages.current = messages;
 
     useEffect(() => {
 
         setMessagesHTML([messages.map((el) => {return renderMessage(el)}) ])
         }, [messages]);
 
-    // useEffect(() => {
 
-    //     },[]);
+    useEffect(() => {
+        const addElements = () => { socket.on("message", msg => {
+            console.log(msg)
+            setMessages([...lastestMessages.current, msg])});
+        };
+        addElements();
+    },[]);
+
+
 // https://stackoverflow.com/questions/54824036/useeffect-hook-with-socket-io-state-is-not-persistent-in-socket-handlers
-
-
-    socket.on("message", msg => {
-        console.log(msg)
-        setMessages([...messages, msg])});
-
-    
 
     const onClick = () => {
         if (message.length > 2){   
